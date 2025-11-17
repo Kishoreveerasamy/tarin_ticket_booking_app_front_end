6deb73e46eb1fa75cb2d1b2bbcb7b3354aee448d
kishoreveerasamy <vkishorek007@gmail.com>
2025-11-17 16:00:27 +0530

    main.jsx update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/train_ticket_booking_app/">
+  <BrowserRouter basename="/tarin_ticket_booking_app_front_end">
     <App />
   </BrowserRouter>
 )
