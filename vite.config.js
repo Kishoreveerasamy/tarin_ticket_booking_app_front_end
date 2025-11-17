7d2bc547cbb8ba333951c418faa54eba1778619b
kishoreveerasamy <vkishorek007@gmail.com>
2025-11-17 15:51:40 +0530

    vite_config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/tarin_ticket_booking_app_front_end/',
   plugins: [react()],
-  base: "/train_ticket_booking_app/",
-});
+})
